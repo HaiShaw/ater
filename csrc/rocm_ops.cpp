051dffdab802d3b4ff476e842594627fa196bdc7
feli <felixamd@163.com>
2025-01-13 11:03:43 +0800

    fix prebuild (#4)
    Co-authored-by: coderfeli <coderfeli@163.com>

--- csrc/rocm_ops.cpp
+++ csrc/rocm_ops.cpp
@@ -3,16 +3,10 @@
 #include "attention_ck.h"
 #include "attention_asm.h"
 #include "cache.h"
-#include "custom_all_reduce.h"
-#include "custom.h"
 #include "moe_op.h"
 #include "moe_sorting.h"
-#include "norm.h"
 #include "pos_encoding.h"
-#include "rmsnorm.h"
 #include "smoothquant.h"
-#include "transpose_operator.h"
-#include "asm_gemm_a8w8.h"
 #include <torch/extension.h>
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
@@ -63,17 +57,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
             "convert_fp8(Tensor! dst_cache, Tensor src_cache, float scale, "
             "str kv_cache_dtype) -> ()");
 
-
-      m.def("dispose", &dispose);
-      m.def("meta_size", &meta_size);
-
-      m.def("register_buffer", &register_buffer,
-            "register_buffer(int fa, Tensor t, str[] handles, "
-            "int[] offsets) -> ()");
-
-      m.def("get_graph_buffer_ipc_meta", &get_graph_buffer_ipc_meta);
-      m.def("register_graph_buffers", &register_graph_buffers);
-
 #if defined(FIND_CK)
       // ck staff start
       m.def("moe_smoothquant_fwd", &moe_smoothquant_fwd);
@@ -100,10 +83,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
       m.def("fmoe", &fmoe);
       m.def("fmoe_int8_g1u0", &fmoe_int8_g1u0);
       m.def("fmoe_int8_g1u0_a16", &fmoe_int8_g1u0_a16);
-      m.def("transpose_add", &transpose_add, "apply for add with transpose.");
-      m.def("transpose_mul", &transpose_mul, "apply for mul with transpose.");
-      m.def("transpose_sub", &transpose_sub, "apply for sub with transpose.");
-      m.def("transpose_div", &transpose_div, "apply for div with transpose.");
       m.def("pa_fwd_asm", &pa_fwd, "pa_fwd",
             py::arg("Q"),
             py::arg("K"),
@@ -112,15 +91,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
             py::arg("context_lens"),
             py::arg("K_QScale") = std::nullopt,
             py::arg("V_QScale") = std::nullopt);
-      m.def("gemm_a8w8_asm", &gemm_a8w8_asm,
-            "Asm gemm a8w8 ,  weight should be shuffle to layout(32,16)",
-            py::arg("XQ"), py::arg("WQ"),
-            py::arg("x_scale"), py::arg("w_scale"),
-            py::arg("Out"), py::arg("bias"),
-            py::arg("sub_m") = 128, py::arg("sub_n") = 128,
-            py::arg("pad_a") = 0, py::arg("pad_b") = 0,
-            py::arg("pad_c") = 0, py::arg("splitK") = 0);
-      m.def("all_reduce_asm", &all_reduce_asm, "");
 
       m.def("reshape_and_cache_with_pertoken_quant", &reshape_and_cache_with_pertoken_quant,
             "reshape_and_cache_with_pertoken_quant(Tensor key, Tensor value,"
